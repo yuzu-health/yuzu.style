486aed2c4a0dc83dd593d3d44f370a74828c7d66
rylegames <rylee.nyc@gmail.com>
2023-07-22 15:45:23 -0400

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import postcss from "rollup-plugin-postcss";
+
+const bundleNames = ["blocks", "reset", "util"];
+
+const configs = bundleNames.map((name) => {
+  return {
+    input: `src/${name}.css`,
+    output: {
+      file: `dist/${name}.min.css`,
+      format: "es",
+    },
+    plugins: [
+      postcss({
+        minimize: true,
+        sourceMap: true,
+        extract: true,
+        modules: false,
+      }),
+    ],
+  };
+});
+
+export default configs;

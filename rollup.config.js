9371126ac9cd9e7daacdb3676d7838740b361e35
rylegames <rylee.nyc@gmail.com>
2023-07-23 22:46:23 -0400

    testing

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import postcss from "rollup-plugin-postcss";
+import autoprefixer from "autoprefixer";
+import postcssPresetEnv from "postcss-preset-env";
+import postcssNesting from "postcss-nesting";
 
 const bundleNames = ["all", "backgrounds", "panels", "themes", "tooltip"];
 
@@ -11,10 +14,11 @@ const configs = bundleNames.map((name) => {
     },
     plugins: [
       postcss({
+        plugins: [postcssNesting, autoprefixer, postcssPresetEnv],
         path: "src",
         modules: false,
         inject: false,
-        minimize: true,
+        // minimize: true,
         extract: true,
         sourceMap: true,
       }),
